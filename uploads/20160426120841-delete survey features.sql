d03af1da254ebe33bda5bd8e8f41bfd3a8a55a82
Amani <amanisaid740@gmail.com>
2025-08-07 15:46:32 +0100

    reomve files from uploads

--- uploads/20160426120841-delete survey features.sql
+++
@@ -1,3 +0,0 @@
-delete from t_answer where ref_question in (select idquestion from t_question where ref_survey=14) and idanswer>10;
-delete from t_question where ref_survey=14 and idquestion>8;
-delete from t_survey where idsurvey=14;
